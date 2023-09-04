374e182625de1c34aad7e566d3c2ba3bdff60789
amandaelfassy1 <amanda.elfassy@outlook.fr>
2023-09-04 16:59:10 +0200

    first-commit

---
+++ src/utility/DB.ts
@@ -0,0 +1,33 @@
+import mysql, { Pool } from 'mysql2/promise';
+
+/** Wrapper de la connexion à la SGBDR.
+ * On stock une seule référence à la connexion-pool, et on va systématiquement
+ * récupérer cette référence pour nos requêtes.
+ */
+export class DB {
+
+  // Variable "static": une seule instance pour toutes les instances de la classe DB
+  private static POOL: Pool | undefined;
+
+  /**
+   * Récupérer ou créer la connexion-pool.
+   */
+  static get Connection(): Pool {
+    if (!this.POOL) {
+      this.POOL = mysql.createPool({
+        host: process.env.DB_HOST || 'dbms',
+        user: process.env.DB_USER || 'api-test',
+        database: process.env.DB_DATABASE || 'school_test',
+        password: process.env.DB_PASSWORD || 'rootpassword',  
+      });
+    }
+
+    return this.POOL;
+  }
+  static async Close() {    
+    if (this.POOL) {      
+      await this.POOL.end();      
+      this.POOL = undefined;    
+    }
+  }
+}
